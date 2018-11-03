2adf4d8ef399b22a5b207a63393582db0caa0e5a
Adam <18970079817@189.cn>
2018-11-03 15:29:36 +0800

    Update Chinese_dlt.py

--- Chinese_dlt.py
+++ Chinese_dlt.py
@@ -10,8 +10,8 @@ else:
     r.encoding = 'utf-8'
     t = r.text
     soup = BeautifulSoup(t, 'lxml')
-    for tr in soup.find_all('tr'):
-        latest = str(tr.find('td'))#Get the latest number from the default page.
+    for tr in soup.find_all('tbody'):
+        latest = tr.find('td').string#Get the latest number from the default page.
     url_a = 'http://www.lottery.gov.cn/historykj/history.jspx?page=false&_ltype=dlt&termNum=0&startTerm=07001&endTerm=' + latest#Inquiry all the past numbers.
 try:
     r_a = requests.get(url_a)
@@ -21,7 +21,7 @@ else:
     r_a.encoding = 'utf-8'
     t_a = r_a.text
     soup_a = BeautifulSoup(t_a, 'lxml')
-    for tr_a in soup_a.find_all('tr_a'):
+    for tr_a in soup_a.find_all('tr'):
         i = 1
         for td in tr_a.find_all('td'):
             if i % 20 == 0:#Every 20 attribute a new line.
